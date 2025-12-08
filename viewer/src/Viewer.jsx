c955c57701aee40770ba66db67a8c4ff849a7ab8
JoshHagg <joshchagglund@gmail.com>
2025-12-08 12:38:25 +0000

    Adjustments to how the viewer displays the simulation

--- viewer/src/Viewer.jsx
+++ viewer/src/Viewer.jsx
@@ -30,6 +30,10 @@ function renderWorld(world) {
   // Use uniform scale so circles stay round
   const scale = Math.min(scaleX, scaleY);
 
+    // calculate centered offsets
+  const offsetX = (screenW - worldW * scale) / 2;
+  const offsetY = (screenH - worldH * scale) / 2;
+
   app.stage.removeChildren();
 
   // Draw food
@@ -38,10 +42,10 @@ if (world.food) {
     const g = new PIXI.Graphics();
     g.beginFill(0xffcc00); // yellow food color
 
-    const sx = f.x * scale;
-    const sy = f.y * scale;
+    const sx = offsetX + f.x * scale;
+    const sy = offsetY + f.y * scale;
 
-    g.drawCircle(sx, sy, 3 * scale);
+    g.drawCircle(sx, sy, 4);
     g.endFill();
 
     app.stage.addChild(g);
@@ -66,15 +70,15 @@ if (world.food) {
   const iy = prev.y + (a.y - prev.y) * lerp;
 
   // Convert to screen space
-  const sx = ix * scale;
-  const sy = iy * scale;
+  const sx = offsetX + ix * scale;
+  const sy = offsetY + iy * scale;
 
   // Update cache
   lastPositions[a.id] = { x: ix, y: iy };
 
     const g = new PIXI.Graphics();
     g.beginFill(0x00ff00);
-    g.drawCircle(sx, sy, 4 * scale); 
+    g.drawCircle(sx, sy, 6); 
     g.endFill();
 
     app.stage.addChild(g);
